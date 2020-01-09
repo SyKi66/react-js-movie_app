f136a9e013a60de3c92e9b0dc89e71ea25a35192
SyKi66 <lusky69@gmail.com>
2020-01-09 15:02:13 +0900

    Updates

---
+++ precache-manifest.b90799ac296925bf68bff774b271e58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba0e7f6d6cf1e78579acd41487a4e1e",
+    "url": "/react-js-movie_app/index.html"
+  },
+  {
+    "revision": "0946d98d178aef48eee7",
+    "url": "/react-js-movie_app/static/css/main.c7dc77d4.chunk.css"
+  },
+  {
+    "revision": "aefae647e1da59f5549c",
+    "url": "/react-js-movie_app/static/js/2.a5ed20a7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-js-movie_app/static/js/2.a5ed20a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0946d98d178aef48eee7",
+    "url": "/react-js-movie_app/static/js/main.9f4d2511.chunk.js"
+  },
+  {
+    "revision": "ab67f16397dc69a1a984",
+    "url": "/react-js-movie_app/static/js/runtime-main.63e8cabe.js"
+  }
+]);
